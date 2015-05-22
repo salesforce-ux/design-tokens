9e1e4f5ef7b4b265ec4dc1f9b248cbd6d2724ffd
Adam Putinski <adam.putinski@salesforce.com>
2015-05-22 15:00:57 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Salesforce Design Tokens
+
+Design Properties for colors, font sizes etc.
+
+## Setup
+
+```bash
+git clone git@github.com:salesforce-ux/design-tokens-internal.git
+cd design-tokens-internal
+npm install
+```
+
+## Generate Design Tokens
+
+```bash
+npm run props
+```
+
+View the `./dist` directory for the generated output
---
+++ gulpfile.js
@@ -0,0 +1,84 @@
+var path = require('path');
+
+var rimraf = require('rimraf');
+var async = require('async');
+var _ = require('lodash');
+
+var gulp = require('gulp');
+var jsonlint = require('gulp-json-lint');
+var rename = require('gulp-rename');
+var theo = require('theo');
+
+////////////////////////////////////////////////////////////////////
+// Helpers
+////////////////////////////////////////////////////////////////////
+
+function clean(f, done) {
+  var p = path.resolve(__dirname, f);
+  rimraf(f, done);
+}
+
+////////////////////////////////////////////////////////////////////
+// Tasks - Clean
+////////////////////////////////////////////////////////////////////
+
+gulp.task('clean', function(done) {
+  async.each(['dist'], clean, done);
+});
+
+////////////////////////////////////////////////////////////////////
+// Tasks - Tokens
+////////////////////////////////////////////////////////////////////
+
+var convertOptions = _({
+  'web': [
+    'styl', 'less', 'scss', 'map.scss', 'html', 'json',
+    'common.js', 'amd.js', 'aura.theme'
+  ],
+  'ios': ['ios.json'],
+  'android': ['android.xml']
+}).map(function(formats, transform) {
+  return formats.map(function(format) {
+    return {
+      format: format,
+      transform: transform
+    };
+  });
+}).flatten().value();
+
+function convert(options, done) {
+  gulp.src([
+    './tokens/*.json', '!./tokens/_*.json'
+  ])
+  .pipe(theo.plugins.transform(options.transform))
+  .on('error', done)
+  .pipe(theo.plugins.format(options.format))
+  .on('error', done)
+  .pipe(gulp.dest(path.resolve(__dirname, 'dist')))
+  .on('error', done)
+  .on('finish', done);
+}
+
+gulp.task('tokens', ['clean', 'lint'], function(done) {
+  async.each(convertOptions, convert, done);
+});
+
+////////////////////////////////////////////////////////////////////
+// Tasks - Lint
+////////////////////////////////////////////////////////////////////
+
+gulp.task('lint', function() {
+  return gulp.src('./tokens/*.json')
+    .pipe(jsonlint({ comments: true }))
+    .pipe(jsonlint.report('verbose'));
+});
+
+////////////////////////////////////////////////////////////////////
+// Tasks
+////////////////////////////////////////////////////////////////////
+
+gulp.task('dev', function() {
+  gulp.watch('./tokens/**', ['tokens']);
+});
+
+gulp.task('default', ['tokens']);
